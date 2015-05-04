52994cd063dfe2b5fce2c4596fca62f88fcc9e22
Jason Madden <jamadden@gmail.com>
2015-05-04 09:23:10 -0500

    Add a Travis CI config

--- setup.py
+++ setup.py
@@ -1,33 +1,34 @@
 from setuptools import setup, find_packages
 import codecs
 
-VERSION = '0.0.0'
+VERSION = '0.0.0.dev0'
 
 entry_points = {
 }
 
 setup(
-    name = 'nti.testing',
-    version = VERSION,
-    author = 'Jason Madden',
-    author_email = 'jason@nextthought.com',
-    description = "Support for testing code",
-    long_description = codecs.open('README.rst', encoding='utf-8').read(),
-    license = 'Proprietary',
-    keywords = 'nose testing',
-    url = 'https://github.com/NextThought/nti.testing',
-    classifiers = [
-        'Intended Audience :: Developers',
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
+	name = 'nti.testing',
+	version = VERSION,
+	author = 'Jason Madden',
+	author_email = 'jason@nextthought.com',
+	description = "Support for testing code",
+	long_description = codecs.open('README.rst', encoding='utf-8').read(),
+	license = 'Proprietary',
+	keywords = 'nose testing',
+	url = 'https://github.com/NextThought/nti.testing',
+	classifiers = [
+		'Intended Audience :: Developers',
+		'Natural Language :: English',
+		'Operating System :: OS Independent',
 		'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Topic :: Software Development :: Testing'
-        ],
+		'Programming Language :: Python :: 2.7',
+		'Topic :: Software Development :: Testing'
+		],
 	packages=find_packages('src'),
 	package_dir={'': 'src'},
 	namespace_packages=['nti'],
 	install_requires=[
+		'Acquisition',
 		'fudge',
 		'nose',
 		'persistent',
