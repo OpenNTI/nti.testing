94663081e319f75c8096f4a271f859cb58cd6128
Jason Madden <jamadden@gmail.com>
2017-04-12 11:02:16 -0500

    release metadata fixups

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Software Development :: Testing',
-        'Framework:: Zope3',
+        'Framework :: Zope3',
         ],
     zip_safe=True,
     packages=find_packages('src'),
