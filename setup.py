f67b9d44e79467abe46f66ea5938dce636b8bc67
Jason Madden <jamadden@gmail.com>
2024-11-06 10:20:59 -0600

    setup.py: fix classifier typo

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
         'Operating System :: OS Independent',
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: Only',
+        'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3.12',
